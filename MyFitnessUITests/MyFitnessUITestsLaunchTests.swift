f7eda1ddf894d30f2a222c365415ff0701510e42
Alex Udrea <alexandru.udrea@student.upt.ro>
2022-09-24 03:01:11 +0300

    Initial Commit

---
+++ MyFitnessUITests/MyFitnessUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyFitnessUITestsLaunchTests.swift
+//  MyFitnessUITests
+//
+//  Created by Udrea Alexandru-Iulian-Alberto on 24.09.2022.
+//
+
+import XCTest
+
+class MyFitnessUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

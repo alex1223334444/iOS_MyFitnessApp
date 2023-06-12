b6670e64bbb0b4a157ec5b160832be80313b2ef3
Alexandru Udrea <alex.udrea@pago.ro>
2023-06-13 00:49:34 +0300

    Added buttons in settings page

--- DailyCalories/ProfileViewController.swift
+++ DailyCalories/ProfileViewController.swift
@@ -6,16 +6,45 @@
 //
 
 import UIKit
+import CoreData
+import FirebaseAuth
 
 class ProfileViewController: UIViewController {
     
-    
+    private var managedObjectContext: NSManagedObjectContext!
+    private var email = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.addHeader(string: "Profile")
+        addLogoutButton()
+        addGoalButton()
+        addDeleteButton()
+        addChangePasswordButton()
+        if let email = UserDefaults.standard.string(forKey: "username") {
+            self.email = email
+        } else {
+            print("email not found")
+        }
+        managedObjectContext = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+        addProfile()
         
-        
+    }
+    
+    fileprivate func addDeleteButton() {
+        let deleteButton = UIButton()
+        self.view.addSubview(deleteButton)
+        deleteButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            deleteButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            deleteButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 150)
+        ])
+        deleteButton.setTitle("Delete account", for: .normal)
+        deleteButton.setTitleColor(.systemRed, for: .normal)
+        deleteButton.addTarget(self, action:#selector(self.deleteAccount), for: .touchUpInside)
+    }
+    
+    fileprivate func addLogoutButton() {
         let logoutButton = UIButton()
         self.view.addSubview(logoutButton)
         logoutButton.translatesAutoresizingMaskIntoConstraints = false
@@ -26,7 +55,65 @@ class ProfileViewController: UIViewController {
         logoutButton.setTitle("Logout", for: .normal)
         logoutButton.setTitleColor(.black, for: .normal)
         logoutButton.addTarget(self, action:#selector(self.logout), for: .touchUpInside)
+    }
+    
+    fileprivate func addProfile() {
+        let pictureView = UIImageView(image:  UIImage(systemName: "person.circle.fill"))
+        pictureView.contentMode = .scaleAspectFill
+        pictureView.tintColor = .systemGreen
+        pictureView.clipsToBounds = true
+        pictureView.layer.cornerRadius = 50
+        view.addSubview(pictureView)
+        pictureView.translatesAutoresizingMaskIntoConstraints = false
         
+        let user = fetchUser()
+        let name = UILabel()
+        if let firstName = user?.firstName, let lastName = user?.lastName {
+            name.text = "\(firstName) \(lastName)"
+        }
+        name.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(name)
+        
+        let emailLabel = UILabel()
+        emailLabel.text = email
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(emailLabel)
+        NSLayoutConstraint.activate([
+            pictureView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            pictureView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            pictureView.heightAnchor.constraint(equalToConstant: 100),
+            pictureView.widthAnchor.constraint(equalToConstant: 100),
+            name.leadingAnchor.constraint(equalTo: pictureView.trailingAnchor, constant: 20),
+            name.topAnchor.constraint(equalTo: view.topAnchor, constant: 220),
+            emailLabel.leadingAnchor.constraint(equalTo: pictureView.trailingAnchor, constant: 20),
+            emailLabel.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 10)
+        ])
+    }
+    
+    fileprivate func addGoalButton() {
+        let goalButton = UIButton()
+        self.view.addSubview(goalButton)
+        goalButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            goalButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            goalButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 100)
+        ])
+        goalButton.setTitle("Add calories goal", for: .normal)
+        goalButton.setTitleColor(.black, for: .normal)
+        goalButton.addTarget(self, action:#selector(self.changeCalorieGoal), for: .touchUpInside)
+    }
+    
+    fileprivate func addChangePasswordButton() {
+        let changePassword = UIButton()
+        self.view.addSubview(changePassword)
+        changePassword.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            changePassword.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            changePassword.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50)
+        ])
+        changePassword.setTitle("Change your password", for: .normal)
+        changePassword.setTitleColor(.black, for: .normal)
+        changePassword.addTarget(self, action:#selector(self.logout), for: .touchUpInside)
     }
     
     @objc private func logout() {
@@ -35,14 +122,49 @@ class ProfileViewController: UIViewController {
         self.performSegue(withIdentifier: "logout", sender: nil)
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    
+    @objc private func changeCalorieGoal() {
+        UserDefaults.standard.removeObject(forKey: "username")
+    }
+    
+    @objc private func deleteAccount() {
+        let user = Auth.auth().currentUser
+        user?.delete { error in
+            if let error = error {
+                print(error)
+            }
+        }
+        let userCoreData = fetchUser()
+        if let user = userCoreData {
+            managedObjectContext.delete(user)
+            do {
+                try managedObjectContext.save()
+            } catch let error as NSError {
+                print("Error deleting object: \(error.localizedDescription)")
+            }
+        }
+        UserDefaults.standard.removeObject(forKey: "username")
+        UserDefaults.standard.removeObject(forKey: "uid")
+        self.performSegue(withIdentifier: "logout", sender: nil)
+        
+    }
+    
+    
+    
+    func fetchUser() -> User? {
+        let fetchRequest: NSFetchRequest<User> = User.fetchRequest()
+
+        // Create a predicate to match the UUID
+        let uuidPredicate = NSPredicate(format: "email == %@", self.email)
+        fetchRequest.predicate = uuidPredicate
+        
+        do {
+            let users = try managedObjectContext.fetch(fetchRequest)
+            return users.first
+        } catch {
+            print("Error fetching user: \(error)")
+            return nil
+        }
+    }
     
 }
--- DailyCalories/SettingsViewController.swift
+++ DailyCalories/SettingsViewController.swift
@@ -29,69 +29,11 @@ class SettingsViewController: UIViewController, TextFieldWithLabelDelegate {
         var request = URLRequest(url: url)
         request.setValue("jWhNwKEikKQQYZPwOq6gkA==EcFrD0XB6Fe8uZ2A", forHTTPHeaderField: "X-Api-Key")
         let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
-         guard let data = data else { return }
-         print(String(data: data, encoding: .utf8)!)
-         }
-         task.resume()
-        /*URLSession.shared.dataTask(with: request) { [self] data, response, error in
-            let foods = try! JSONDecoder().decode([FoodRequested].self, from: data!)
-            //print(foods)
-            for i in 0..<foods.count{
-                let myFood = Food(uid: uid, name: foods[i].name, calories: foods[i].calories,serving_size_g: foods[i].serving_size_g, fat_total_g: foods[i].fat_total_g, fat_saturated_g: foods[i].fat_saturated_g, protein_g: foods[i].protein_g, sodium_mg: foods[i].sodium_mg, potassium_mg: foods[i].potassium_mg, cholesterol_mg: foods[i].cholesterol_mg, carbohydrates_total_g: foods[i].carbohydrates_total_g, fiber_g: foods[i].fiber_g, sugar_g: foods[i].sugar_g)
-                print(myFood)
-                requestedFood?.append(myFood)
-                addFood(food: myFood, completion: { result in
-                    switch result {
-                    case .success(_):
-                        print("success")
-                    case .failure(let error):
-                        print(error)
-                    }
-                })
-            }
-            
-            print(self.requestedFood as Any)
-        }.resume()
-        */
-        
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        /*self.view.gradientBackground(from: .magenta,to: .orange, direction: .bottomToTop)
-        self.submitButton.translatesAutoresizingMaskIntoConstraints = false
-        self.submitButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
-        self.submitButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        self.submitButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32).isActive = true
-        self.submitButton.heightAnchor.constraint(equalToConstant: 40).isActive = true*/
-        // Do any additional setup after loading the view.
-    }
-    
-    
-    
-   /* override func viewWillAppear(_ animated: Bool) {
-        if let user = Auth.auth().currentUser {
-            let id = user.uid
-            uid = id
-            print(id)
-            getFoods(forUserWithId: uid) { (foods, error) in
-                if let error = error {
-                    // Handle the error
-                    print(error)
-                    return
-                }
-
-                if let foods = foods {
-                    self.userFood = foods
-                    print(self.userFood)
-                }
-            }
-        } else {
-            //
+            guard let data = data else { return }
+            print(String(data: data, encoding: .utf8)!)
         }
+        task.resume()
         
         
     }
-    */
-    
 }
